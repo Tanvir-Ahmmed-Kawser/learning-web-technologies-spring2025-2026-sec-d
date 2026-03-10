38edd730ec5aede9a766423c791f07361b66c0eb
Tanvir Ahmmed Kawser <tanvir.kawser.58@gmail.com>
2026-03-10 14:37:16 +0600

    HTML + CSS

---
+++ Tic-Tac-Toe/game.js
@@ -0,0 +1,7 @@
+//alert("working");
+let cell = document.getElementsByTagName("td");
+let reset = document.getElementById("reset");
+let role = document.getElementById("role");
+
+let player = "X";
+let game = ["","","","","","","","",""];
