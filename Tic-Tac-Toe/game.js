4e4be32380a81f20c51b4a110aead8e9c374bdf9
Tanvir Ahmmed Kawser <tanvir.kawser.58@gmail.com>
2026-03-10 15:03:28 +0600

    js

--- Tic-Tac-Toe/game.js
+++ Tic-Tac-Toe/game.js
@@ -5,3 +5,35 @@ let role = document.getElementById("role");
 
 let player = "X";
 let game = ["","","","","","","","",""];
+
+for(let i = 0; i < cell.length; i++){
+    cell[i].addEventListener("click", function(){
+
+        if(game[i] !==""){
+            return;
+        }
+        game[i] = player;
+        cell[i].innerText = player;
+
+       // checkWinner();
+
+        if(player === "X"){
+            player = "O";
+        } 
+        else {
+            player = "X";
+        }
+
+        role.innerText = player + " turns";
+    });     
+}
+
+reset.addEventListener("click", function(){
+    game = ["","","","","","","","",""];
+    for(let i = 0; i < cell.length; i++){
+        cell[i].innerText = "";
+    }
+    player = "X";
+    role.innerText = "X Turn";
+
+});
