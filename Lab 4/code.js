8610b9fefa7abaaf25afe263b5c0e5b75bc5fcb2
Tanvir Ahmmed Kawser <tanvir.kawser.58@gmail.com>
2026-03-15 14:34:31 +0600

    Updating js file

---
+++ Lab 4/code.js
@@ -0,0 +1,23 @@
+const price = 1000;
+
+let quantity = document.getElementById("Quantity");
+let totalPrice = document.getElementById("totalPrice");
+
+quantity.addEventListener("input", function(){
+
+    let q = parseInt(quantity.value);
+
+    if(q < 0){
+        q = 0;
+        quantity.value = 0;
+    }
+
+    let total = price * q;
+
+    totalPrice.textContent = total;
+
+    if (total > 1000){
+        alert("You are now eligible for gift coupon");
+    }
+
+});
