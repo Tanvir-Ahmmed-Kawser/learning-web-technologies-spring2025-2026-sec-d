2116c08fa12bc47e552ec0cd2e97dbcc026c37c4
Tanvir Ahmmed Kawser <tanvir.kawser.58@gmail.com>
2026-04-02 08:13:22 +0600

    Update

--- Calculator/Calculator.js
+++ Calculator/Calculator.js
@@ -1,1 +1,48 @@
-//alert("Calculator");
+let display = document.getElementById("Display");
+let currentInput = "";
+
+function updateDisplay() {
+    display.innerText = currentInput;
+}
+
+for (let i = 0; i <= 9; i++) {
+    document.getElementById("btn" + i).onclick = function () {
+        currentInput += i;
+        updateDisplay();
+    };
+}
+
+document.getElementById("opDiv").onclick = function () {
+    currentInput += "/";
+    updateDisplay();
+};
+
+document.getElementById("opMul").onclick = function () {
+    currentInput += "*";
+    updateDisplay();
+};
+
+document.getElementById("opSub").onclick = function () {
+    currentInput += "-";
+    updateDisplay();
+};
+
+document.getElementById("opAdd").onclick = function () {
+    currentInput += "+";
+    updateDisplay();
+};
+// Equal button
+document.getElementById("equal").onclick = function () {
+    try {
+        currentInput = eval(currentInput).toString();
+        updateDisplay();
+    } catch {
+        display.innerText = "Error";
+        currentInput = "";
+    }
+};
+
+document.getElementById("Clear").onclick = function () {
+    currentInput = "";
+    updateDisplay();
+};
