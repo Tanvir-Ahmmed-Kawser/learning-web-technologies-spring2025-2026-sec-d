ccfb3b740da5c05fb29df9b468751c0e2189892b
Tanvir Ahmmed Kawser <tanvir.kawser.58@gmail.com>
2026-04-21 16:21:49 +0600

    Minimizing mid term task and upload final task 1

---
+++ Final Lab Task 1/1/index.php
@@ -0,0 +1,14 @@
+<?php
+$username = $_POST["userName"];
+$password = $_POST["password"];
+
+// echo $username;
+// echo $password;
+
+// print_r($_POST);
+if(empty($username))
+    {
+        
+    }
+
+?>
